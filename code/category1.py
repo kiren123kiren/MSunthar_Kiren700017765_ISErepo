fb19d60618a423b5853b4f61c7126981f6ff47d8
kiren123kiren <kiren12345kiren@gmail.com>
2022-05-30 17:17:01 +0800

    Adding Module D1 and module input option

--- code/category1.py
+++ code/category1.py
@@ -1,84 +1,72 @@
 
 #Module A1
 #Function: Converting a string to upper case or lower case
-def ModuleA1():
+def ModuleA1(module_output):
 	print("1: Convert a string to upper case")
 	print("2: Convert a string to lower case")
 	selection_input = input()
-
-	print("1: Input with keyboard")
-	print("2: Input with text file")
-	input_method = input()
-
-	if int(input_method) == 1:
-		print("Please enter a string:")
-		input_text = input()
-	elif int(input_method) == 2:
-		input_text = ModuleE1()
+	if(!selection_input.isdigit()):
+		print("Please enter digit number")
+		return
+	
+	if module_output == "":
+		input_text = input_option()
+				
 	else:
-		print("Please enter a valid selection")		
-
-	if int(selection_input) == 1:
-		output_text = input_text.upper()
-		print("The Upper-case of {0} is {1}".format(input_text,output_text))
-	elif int(selection_input) == 2:
-		output_text = input_text.lower()
-		print("The Lower-case of {0} is {1}".format(input_text,output_text)) 
-	else:
-		print("Please select available selection!")
-		output_text = ""
-		
-	ModuleF1(output_text)
+		input_text = module_output
+	
+	if input_text != "":
+		if int(selection_input) == 1:
+			output_text = input_text.upper()
+			print("Upper case: {0}".format(output_text))
+		elif int(selection_input) == 2:
+			output_text = input_text.lower()
+			print("Lower case: {0}".format(output_text)) 
+		else:
+			print("Please select available selection!")
+			output_text = ""	
+		ModuleF1(output_text)
 
 #Module B1
 #Function: Identify whether numeric values are in a given string
 def ModuleB1():
-	print("1: Input with keyboard")
-	print("2: Input with text file")	
-	input_method = input()
-	
-	if int(input_method) == 1:
-		print("Please enter a string:")
-		input_text = input()
-	elif int(input_method) == 2:
-		input_text = ModuleE1()
-	else:
-		print("Please enter a valid selection")
-	
-	
-	result = any(map(str.isdigit,input_text))
-	if result == True:
-		output_text = "The given string contain numeric values"
-		print(output_text)
-	else:
-		output_text = "The given string DOES NOT contain numeric values"
-		print(output_text)
-	ModuleF1(output_text)
+	input_text = input_option()
+	if input_text != "":
+		result = any(map(str.isdigit,input_text))
+		if result == True:
+			output_text = "The given string contain numeric values"
+			print(output_text)
+		else:
+			output_text = "The given string DOES NOT contain numeric values"
+			print(output_text)
+		ModuleF1(output_text)
 
 #Module C1
 #Function: Identify whether a given string is a valid number or not
 def ModuleC1():
-	print("1: Input with keyboard")
-	print("2: Input with text file")
-	input_method = input()
+	input_text = input_option()
+	if input_text != "":
+		result = input_text.isdigit()
+		if result == True:
+			output_text = "The given string is a valid number"
+			print(output_text)
+		else:
+			output_text = "The given string is NOT a valid number"
+			print(output_text)
+		ModuleF1(output_text)
 
-	if int(input_method) == 1:
-		print("Please enter a string:")
-		input_text = input()
-	elif int(input_method) == 2:
-		input_text = ModuleE1()
-	else:
-		print("Please enter a valid selection")
-	
-	result = input_text.isdigit()
-	if result == True:
-		output_text = "The given string is a valid number"
-		print(output_text)
-	else:
-		output_text = "The given string is NOT a valid number"
-		print(output_text)
-	ModuleF1(output_text)
-
+#Module D1
+#Function: Remove any numeric values in a given string and then convert the string to upper case or lower case
+def ModuleD1():
+	input_text = input_option()
+	if input_text != "":
+		output_text = ""
+		for c in input_text:
+			if c.isdigit():
+				continue
+			else:
+				output_text += c
+		return output_text
 
 #Module E1
 #Function: Input value with a text file
@@ -99,21 +87,41 @@ def ModuleF1(output_text):
 	finally:
 		f.close()
 
+#Module Input Option
+#Function: Allow user to select specific input method
+def input_option():
+	print("1: Input with keyboard")
+	print("2: Input with text file")
+	input_method = input()
+	input_text = ""
+	if int(input_method) == 1:
+		print("Please enter a string:")
+		input_text = input()
+	elif int(input_method) == 2:
+		input_text = ModuleE1()
+	else:
+		print("Please enter a valid selection")
+	return input_text
+	
 #main_part
 print("Please select function that wanted to perform:")
 print("1: Converting a string to upper case or lower case")
 print("2: Identify whether numeric values are in a given string")
 print("3: Identify whether a given string is a valid number or not")
+print("4: Remove any numeric values in a given string and then convert the string to upper case or lower case")
 
 print("Select?:")
 select_func = input()
 
 if int(select_func) == 1:
-	ModuleA1()
-elif int(select_func) ==2:
+	ModuleA1("")
+elif int(select_func) == 2:
 	ModuleB1()
-elif int(select_func) ==3:
+elif int(select_func) == 3:
 	ModuleC1()
+elif int(select_func) == 4:
+	module_output = ModuleD1()
+	ModuleA1(module_output)
 else:
 	print("Please select a valid Number!")
 
