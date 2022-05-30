31e5d1a83b1e039977e8606006cd20266c6d883c
kiren123kiren <kiren12345kiren@gmail.com>
2022-05-30 20:07:44 +0800

    Adding ModuleE2 by input using text file

--- code/category2.py
+++ code/category2.py
@@ -2,13 +2,24 @@
 #Function: Converting a number given in hours to minutes
 def ModuleA2():
 	print("Please enter hours that needed to convert to minutes: ")
-	input_text = input()
-	if not input_text.replace(".","",1).isdigit():
-		print("Please enter a valid hours")
-		return
-	else:
-		result = float(input_text) * 60
-		print("{0} hours => {1} minutes".format(input_text,result))
+	for input_text in ModuleE2():
+		input_text = input_text.rstrip('\n')
+		if not input_text.replace(".","",1).isdigit():
+			print("Please enter valid hours")
+			return
+		else:
+			result = float(input_text) * 60
+			print("{0} hours => {1} minutes".format(input_text,result))
+
+#Module E2
+#Function: Input value with a text file
+def ModuleE2():
+	try:
+		f = open("../document/inCategory2.txt","r")
+		for line in f:
+			yield line
+	finally:
+		f.close()
 
 #main part
 ModuleA2()
