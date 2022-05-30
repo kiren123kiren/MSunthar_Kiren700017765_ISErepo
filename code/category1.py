4e89d52205e13d0d2ee502ba07c37c3075f94d5e
kiren123kiren <kiren12345kiren@gmail.com>
2022-05-30 13:56:37 +0800

    Created ModuleA1 with lower and upper case selection

--- code/category1.py
+++ code/category1.py
@@ -1,13 +1,20 @@
 #Module A1
 #Function: Converting a string to upper case or lower case
-def ModuleA1(text):
-	new_text = text.upper()
-	return new_text
+def ModuleA1():
+	print("Convert a string to upper case, Press 1")
+	print("Convert a string to lower case, Press 2")
+	selection_input = input()
+	print("Please enter a string:")
+	input_text = input()
 
+	if int(selection_input) == 1:
+		output_text = input_text.upper()
+		print("The Upper-case of {0} is {1}".format(input_text,output_text))
+	elif int(selection_input) == 2:
+		output_text = input_text.lower()
+		print("The Lower-case of {0} is {1}".format(input_text,output_text)) 
+	else:
+		print("Please select available selection!")
 #main_part
-print("Please enter a string\n")
-input_text = input()
-
-output_text = ModuleA1(input_text)
-print("Upper case text:{0}".format(output_text))
+ModuleA1()
 
