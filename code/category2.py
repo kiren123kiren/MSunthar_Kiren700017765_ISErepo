74bcf7cdc9ea8363533e6c3fef90042cdee882a7
kiren123kiren <kiren12345kiren@gmail.com>
2022-05-30 21:28:19 +0800

    Created ModuleC2

--- code/category2.py
+++ code/category2.py
@@ -29,7 +29,22 @@ def ModuleB2():
 			output_file.write(str(result)+"\n")
 			print("{0} minutes => {1} hours".format(input_text,result))
 	output_file.close()
-	
+
+#Module C2
+#Function: Converting a number given in minutes to seconds
+def ModuleC2():
+	output_file = open("../document/outCategory2.txt","w")
+	for input_text in ModuleE2():
+		input_text = input_text.rstrip('\n')
+		if not input_text.replace(".","",1).isdigit():
+			print("Please enter valid minutes")
+			return	
+		else:
+			result = float(input_text) * 60
+			result = round(result,4)
+			output_file.write(str(result) + "\n")
+			print("{0} minutes => {1} seconds".format(input_text,result))
+	output_file.close()
 
 #Module E2
 #Function: Input value with a text file
@@ -45,6 +60,7 @@ def ModuleE2():
 print("Please select conversion that needed:")
 print("1: Converting a number given in hours to minutes")
 print("2: Converting a number given in minutes to hours")
+print("3: Converting a number given in minutes to seconds")
 select_func = input()
 if not select_func.isdigit():
 	print("Please enter a valid digit number!")
@@ -53,5 +69,7 @@ else:
 		ModuleA2()
 	elif int(select_func) == 2:
 		ModuleB2()
+	elif int(select_func) == 3:
+		ModuleC2()
 	else:
 		print("Please select a valid option")
