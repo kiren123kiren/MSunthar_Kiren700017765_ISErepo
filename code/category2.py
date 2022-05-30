cfea12d5f8d1485e180c6ffbf6bb3dc8df883e13
kiren123kiren <kiren12345kiren@gmail.com>
2022-05-30 19:41:20 +0800

    Convert var to float before conducting calculation

--- code/category2.py
+++ code/category2.py
@@ -1,12 +1,14 @@
 #Module A2
 #Function: Converting a number given in hours to minutes
 def ModuleA2():
+	print("Please enter hours that needed to convert to minutes: ")
 	input_text = input()
-	if not input_text.isdigit():
+	if not input_text.replace(".","",1).isdigit():
 		print("Please enter a valid hours")
 		return
 	else:
-		result = input_text * 60
+		result = float(input_text) * 60
+		print("{0} hours => {1} minutes".format(input_text,result))
 
 #main part
 ModuleA2()
