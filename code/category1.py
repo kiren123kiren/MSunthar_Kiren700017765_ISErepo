4cb09cd497285067cd6218aa5894a9cd869127e5
kiren123kiren <kiren12345kiren@gmail.com>
2022-05-30 14:19:50 +0800

    Adding input function as ModuleE1

--- code/category1.py
+++ code/category1.py
@@ -4,8 +4,18 @@ def ModuleA1():
 	print("Convert a string to upper case, Press 1")
 	print("Convert a string to lower case, Press 2")
 	selection_input = input()
-	print("Please enter a string:")
-	input_text = input()
+
+	print("Input with keyboard, Press 1")
+	print("Input with text file, Press 2")
+	input_method = input()
+
+	if int(input_method) == 1:
+		print("Please enter a string:")
+		input_text = input()
+	elif int(input_method) == 2:
+		input_text = ModuleE1()
+	else:
+		print("Please enter a valid selection")		
 
 	if int(selection_input) == 1:
 		output_text = input_text.upper()
@@ -15,6 +25,16 @@ def ModuleA1():
 		print("The Lower-case of {0} is {1}".format(input_text,output_text)) 
 	else:
 		print("Please select available selection!")
+
+#Module E1
+#Function: Input value with a text file
+def ModuleE1():
+	try:
+		f=open("../document/InCategory1.txt")
+		input_text = f.read()
+		return input_text
+	finally:
+		f.close()
 #main_part
 ModuleA1()
 
