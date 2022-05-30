4c329ea9225e76d00d9c0409e17166ea97af84bc
kiren123kiren <kiren12345kiren@gmail.com>
2022-05-30 21:19:52 +0800

    Created ModuleB2 and able to round off decimal places for output

--- code/category2.py
+++ code/category2.py
@@ -9,10 +9,27 @@ def ModuleA2():
 			return
 		else:
 			result = float(input_text) * 60
+			result = round(result,4)
 			output_file.write(str(result)+"\n")
-			firstLoop = False
 			print("{0} hours => {1} minutes".format(input_text,result))
 	output_file.close()
+
+#Module B2
+#Function: Converting a number given in minutes to hours
+def ModuleB2():
+	output_file = open("../document/outCategory2.txt","w")
+	for input_text in ModuleE2():
+		input_text = input_text.rstrip('\n')
+		if not input_text.replace(".","",1).isdigit():
+			print("Please enter valid minutes")
+			return
+		else:
+			result = float(input_text) / 60
+			result = round(result,4)
+			output_file.write(str(result)+"\n")
+			print("{0} minutes => {1} hours".format(input_text,result))
+	output_file.close()
+	
 
 #Module E2
 #Function: Input value with a text file
@@ -25,4 +42,16 @@ def ModuleE2():
 		f.close()
 
 #main part
-ModuleA2()
+print("Please select conversion that needed:")
+print("1: Converting a number given in hours to minutes")
+print("2: Converting a number given in minutes to hours")
+select_func = input()
+if not select_func.isdigit():
+	print("Please enter a valid digit number!")
+else:
+	if int(select_func) == 1:
+		ModuleA2()
+	elif int(select_func) == 2:
+		ModuleB2()
+	else:
+		print("Please select a valid option")
